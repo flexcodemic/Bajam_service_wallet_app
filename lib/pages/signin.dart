c3faf508f336cc2ed195fdd97ffd6fd424733aea
Anthony Aruma <anthonyaruma091@gmail.com>
2024-09-27 15:14:29 +0100

    Implemented a sign in page or screen for signing in to dashboard

---
+++ lib/pages/signin.dart
@@ -0,0 +1,281 @@
+import 'package:bajam_app/pages/home.dart';
+import 'package:bajam_app/pages/signup.dart';
+import 'package:bajam_app/theme.dart';
+import 'package:icons_plus/icons_plus.dart';
+import 'package:bajam_app/widgets/custom_scafford.dart';
+import 'package:flutter/material.dart';
+
+class SignInScreen extends StatefulWidget {
+  const SignInScreen({super.key});
+  @override
+  State<SignInScreen> createState() => _SignInScreenState();
+}
+
+class _SignInScreenState extends State<SignInScreen> {
+  final _formSignInKey = GlobalKey<FormState>();
+  bool rememberPassword = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return CustomScafford(
+      child: Column(
+        children: <Widget>[
+          const Expanded(
+              flex: 1,
+              child: SizedBox(
+                height: 10,
+              )),
+          Expanded(
+            flex: 10,
+            child: Container(
+              padding: const EdgeInsets.fromLTRB(25.0, 20.0, 25.0, 20.0),
+              decoration: const BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(40.0),
+                    topRight: Radius.circular(40.0)),
+              ),
+              child: Form(
+                key: _formSignInKey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    const Text(
+                      'Sign In',
+                      style: TextStyle(
+                        fontSize: 30.0,
+                        fontWeight: FontWeight.w900,
+                        color: AppColors.primary,
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter Email';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        label: const Text('Email'),
+                        hintText: 'Enter email...',
+                        hintStyle: const TextStyle(color: AppColors.grey),
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: AppColors.grey,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: AppColors.grey,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    TextFormField(
+                      obscureText: true,
+                      obscuringCharacter: '*',
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'Please enter Password';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                        label: const Text('Password'),
+                        hintText: 'Enter password...',
+                        hintStyle: const TextStyle(
+                          color: AppColors.grey,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(
+                            color: AppColors.grey,
+                          ),
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: <Widget>[
+                            Checkbox(
+                              value: rememberPassword,
+                              onChanged: (bool? value) {
+                                setState(() {
+                                  rememberPassword = value!;
+                                });
+                              },
+                              activeColor: AppColors.primary,
+                            ),
+                            const Text(
+                              'Remember me',
+                              style: TextStyle(
+                                color: AppColors.darkgrey,
+                              ),
+                            ),
+                          ],
+                        ),
+                        GestureDetector(
+                          child: const Text(
+                            'Forgot Password?',
+                            style: TextStyle(
+                              color: AppColors.primary,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        onPressed: () {
+                          if (_formSignInKey.currentState!.validate() &&
+                              rememberPassword) {
+                            ScaffoldMessenger.of(context)
+                                .showSnackBar(const SnackBar(
+                              content: Text('Processing...'),
+                            ));
+                          } else if (!rememberPassword) {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text('I agree'),
+                              ),
+                            );
+                          }
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const HomeScreen(),
+                            ),
+                          );
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: AppColors.primary,
+                          foregroundColor: AppColors.white,
+                        ),
+                        child: const Text(
+                          'Sign in',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 25,
+                    ),
+                    const Row(
+                      children: <Widget>[
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.7,
+                            color: AppColors.darkgrey,
+                          ),
+                        ),
+                        Padding(
+                          padding: EdgeInsets.symmetric(
+                            vertical: 0,
+                            horizontal: 10,
+                          ),
+                          child: Text(
+                            'Sign in with',
+                            style: TextStyle(
+                              color: AppColors.darkgrey,
+                            ),
+                          ),
+                        ),
+                        Expanded(
+                          child: Divider(
+                            thickness: 0.7,
+                            color: AppColors.darkgrey,
+                          ),
+                        )
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: <Widget>[
+                        Logo(
+                          Logos.facebook_f,
+                          size: 20,
+                        ),
+                        Logo(
+                          Logos.twitter,
+                          size: 20,
+                        ),
+                        Logo(
+                          Logos.google,
+                          size: 20,
+                        ),
+                        Logo(
+                          Logos.apple,
+                          size: 20,
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        const Text(
+                          'Don\'t have an account? ',
+                          style: TextStyle(
+                            color: AppColors.primary,
+                          ),
+                        ),
+                        GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (e) => const SignUpScreen()),
+                            );
+                          },
+                          child: const Text(
+                            'Sign up',
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              color: AppColors.primary,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
